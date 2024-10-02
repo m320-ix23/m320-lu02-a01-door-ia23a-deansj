e96a6089a3916f7c540eed90e1a951fcfa180a4a
Jasper12343 <jasperdeans@gmail.com>
2024-10-02 10:50:19 +0200

    init 7

--- door.py
+++ door.py
@@ -1,66 +1,66 @@
 class Door:
-    def __init__(self, ref2door_lock, base_color):
-        self.door_is_locked = self.lock_the_door
-        self._the_door_lock = ref2door_lock
-        self.color = base_color
-        self._door_is_open = False
-        self._door_is_locked = False
+  def __init__(self, ref2door_lock, base_color):
+    self.door_is_locked = self.lock_the_door
+    self._the_door_lock = ref2door_lock
+    self.color = base_color
+    self._door_is_open = False
+    self._door_is_locked = False
 
-    def open_the_door(self):
-        if self._door_is_locked is False:
-            self._door_is_open = True
+  def open_the_door(self):
+    if self._door_is_locked is False:
+      self._door_is_open = True
 
-    def close_the_door(self):
-        self._door_is_open = False
+  def close_the_door(self):
+    self._door_is_open = False
 
-    def lock_the_door(self):
-        if self._door_is_open is False:
-            self._door_is_locked = self._the_door_lock.lock()
+  def lock_the_door(self):
+    if self._door_is_open is False:
+      self._door_is_locked = self._the_door_lock.lock()
 
-    def unlock_the_door(self):
-        if self._door_is_locked:
-            self._door_is_locked = self._the_door_lock.unlock()
+  def unlock_the_door(self):
+    if self._door_is_locked:
+      self._door_is_locked = self._the_door_lock.unlock()
 
-    def test(self):
-        print(f'Türfarbe {self.color}'
-              f'Türe offen: {self._door_is_open}'
-              f'Türe verriegelt: {self._door_is_locked}')
+  def test(self):
+    print(f'Türfarbe {self.color}'
+       f'Türe offen: {self._door_is_open}'
+       f'Türe verriegelt: {self._door_is_locked}')
 
-    @property
-    def door_is_open(self):
-        return self._door_is_open
+  @property
+  def door_is_open(self):
+    return self._door_is_open
 
-    @property
-    def door_ist_locked(self):
-        return self._door_is_locked
+  @property
+  def door_ist_locked(self):
+    return self._door_is_locked
 
-    @property
-    def color(self):
-        return self._color
+  @property
+  def color(self):
+    return self._color
 
-    @color.setter
-    def color(self, new_color):
-        self._color = new_color
+  @color.setter
+  def color(self, new_color):
+    self._color = new_color
 
 
 class DoorLock:
 
-    def __init__(self):
-        print("ein Schloss erzeugt")
+  def __init__(self):
+    print("ein Schloss erzeugt")
 
-    def lock(self):
-        return True
+  def lock(self):
+    return True
 
-    def unlock(self):
-        return False
+  def unlock(self):
+    return False
 
 
 # Hier die main-Methode festlegen
 if __name__ == "__main__":
-    print("Test für Tür-Objekt")
-    the_door_lock = DoorLock()
-    the_door = Door(the_door_lock, "grün")
-    the_door.test()
-    print("-- Türe jetzt öffnen")
-    the_door.open_the_door()
-    the_door.test()
+  print("Test für Tür-Objekt")
+  the_door_lock = DoorLock()
+  the_door = Door(the_door_lock, "grün")
+  the_door.test()
+  print("-- Türe jetzt öffnen")
+  the_door.open_the_door()
+  the_door.test()
